72ef3e0d516eb57cdf94e92a0d0dfa83fca39397
Nikolay Namsaraev <newbutt@yandex.ru>
2022-11-10 10:40:07 +0800

    feat: add func lab12

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,6 @@
-//! Run with
-//!
-//! ```not_rust
-//! cd examples && cargo run -p example-readme
-//! ```
-
-use axum::{
-    http::StatusCode,
-    response::IntoResponse,
-    routing::{get, post},
-    Json, Router,
-};
-use serde::{Deserialize, Serialize};
+use axum::{extract::Path, http::StatusCode, response::IntoResponse, routing::get, Json, Router};
+use serde::Serialize;
+use serde_json::json;
 use std::net::SocketAddr;
 
 #[tokio::main]
@@ -20,10 +10,8 @@ async fn main() {
 
     // build our application with a route
     let app = Router::new()
-        // `GET /` goes to `root`
         .route("/", get(root))
-        // `POST /users` goes to `create_user`
-        .route("/users", post(create_user));
+        .route("/v1/todos/:id", get(get_json));
 
     // run our app with hyper
     // `axum::Server` is a re-export of `hyper::Server`
@@ -40,31 +28,21 @@ async fn root() -> &'static str {
     "Hello, World!"
 }
 
-async fn create_user(
-    // this argument tells axum to parse the request body
-    // as JSON into a `CreateUser` type
-    Json(payload): Json<CreateUser>,
-) -> impl IntoResponse {
-    // insert your application logic here
-    let user = User {
-        id: 1337,
-        username: payload.username,
-    };
+async fn get_json(Path(id): Path<String>) -> impl IntoResponse {
+    let message_id = id.as_str();
 
-    // this will be converted into a JSON response
-    // with a status code of `201 Created`
-    (StatusCode::CREATED, Json(user))
+    (
+        StatusCode::OK,
+        Json(json!({"id": message_id, "message": "Just do it!",
+    "priority": "A",
+    "assigned": "user@example.com"})),
+    )
 }
 
-// the input to our `create_user` handler
-#[derive(Deserialize)]
-struct CreateUser {
-    username: String,
-}
-
-// the output to our `create_user` handler
 #[derive(Serialize)]
 struct User {
-    id: u64,
-    username: String,
+    id: String,
+    message: String,
+    priority: String,
+    assigned: String,
 }
